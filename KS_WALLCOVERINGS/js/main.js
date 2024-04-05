1eaa36f8e8939dea87768879bfeadb2d540eb81b
jexxepinkman <157003294+jexxepinkman@users.noreply.github.com>
2024-04-05 09:32:33 +0900

    fdsf
    sdf

--- KS_WALLCOVERINGS/js/main.js
+++ KS_WALLCOVERINGS/js/main.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
-    const exhibit_swiper = new Swiper('.exhibit .swiper', {
 
-	effect: "fade",
+    $('header .inner .gnb ul.depth1 >li').on('mouseenter focusin', function(){
+        $('.header .inner .gnb ul.depth1 >li').removeClass('on')
+        $('header .inner .gnb ul.depth1 >li').addClass('on')
+        $('header').addClass('menu_over')
+    })
+    $('header').on('mouseleave', function(){
+        $('header .inner .gnb ul.depth1 >li').removeClass('on')
+        $('header .inner .gnb ul.depth1 >li').removeClass('on')
+        $('header').removeClass('menu_over')
+    })
+    $('header').on('mouseleave', function(){
+        $('header .inner .gnb ul.depth1 >li').removeClass('on')
+        $('header').removeClass('menu_over')
+    })
 
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 3300,
+    const visual_swiper = new Swiper('.visual .swiper', {
+
+	autoplay: {
+		delay: 2500,
 		disableOnInteraction: true,
 	},
 
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	loop: true,
 
-});
-
-    const sns_swiper = new Swiper('.sns .swiper', {
-	slidesPerView: "auto",
-	spaceBetween: 30,
-	breakpoints: {
-		320: {
-			spaceBetween: 60, 
-		},
-		1025: {
-			spaceBetween: 100,
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
 		},
 	},
-	centeredSlides: false,
-	loop: true,
+
 });
+
 })
